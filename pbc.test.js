5bef99905f6496689a8cd3fc7361297b959fdfcf
SelrahcD <c.desneuf@gmail.com>
2025-01-02 19:33:02 +0100

    Rename function

--- pbc.test.js
+++ pbc.test.js
@@ -7,7 +7,7 @@ test('adds 1 + 2 to equal 3', () => {
 // Baseline is 10 points for now
 
 
-const pbc = (data) => {
+const computePBC = (data) => {
     let result = {
         MOVING_RANGE: [],
         AVERAGE: [],
@@ -48,17 +48,16 @@ const pbc = (data) => {
 
 
     return result
-
 }
 
 describe('Compute the Moving Range between two measurements', () => {
     test('have Moving Range to the result object', () => {
-        const result = pbc([]);
+        const result = computePBC([]);
         expect(result).toHaveProperty('MOVING_RANGE');
     })
 
     test('Doesnt add value for first row', () => {
-        const result = pbc([1]);
+        const result = computePBC([1]);
         expect(result.MOVING_RANGE).toStrictEqual([""]);
     })
 
@@ -68,7 +67,7 @@ describe('Compute the Moving Range between two measurements', () => {
         {data: [0, 1, 0], expected: ["", 1, 1]},
         {data: [0, 1, 10, 5, 6, 3], expected: ["", 1, 9, 5, 1, 3]},
     ])('Value is absolute value of difference between current and previous measurements', ({data, expected}) => {
-        const result = pbc(data);
+        const result = computePBC(data);
         expect(result.MOVING_RANGE).toStrictEqual(expected);
 
     })
@@ -77,7 +76,7 @@ describe('Compute the Moving Range between two measurements', () => {
 
 describe('Compute the Average for the baseline', () => {
     test('have Average to the result object', () => {
-        const result = pbc([]);
+        const result = computePBC([]);
         expect(result).toHaveProperty('AVERAGE');
     })
 
@@ -87,14 +86,14 @@ describe('Compute the Average for the baseline', () => {
         {data: [1, 3], expected: [2, 2]},
         {data: [1, -1], expected: [0, 0]},
     ])('Average value is the average of the measurements in the baseline', ({data, expected}) => {
-        const result = pbc(data);
+        const result = computePBC(data);
         expect(result.AVERAGE).toStrictEqual(expected);
     })
 
     test.each([
         {data: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 100], expected: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]},
     ])('Average doesnt use value outside of the baseline', ({data, expected}) => {
-        const result = pbc(data);
+        const result = computePBC(data);
         expect(result.AVERAGE).toStrictEqual(expected);
     })
 
@@ -102,7 +101,7 @@ describe('Compute the Average for the baseline', () => {
 
 describe('Compute the Average Moving Range for the baseline', () => {
     test('have Average Moving Range to the result object', () => {
-        const result = pbc([]);
+        const result = computePBC([]);
         expect(result).toHaveProperty('AVERAGE_MOVING_RANGE');
     })
 
@@ -114,7 +113,7 @@ describe('Compute the Average Moving Range for the baseline', () => {
         {data: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 10], expected: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]},
         {data: [1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 10], expected: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]},
     ])('Average Moving Range value is the average of the moving range between two measurements in the baseline', ({data, expected}) => {
-        const result = pbc(data);
+        const result = computePBC(data);
         expect(result.AVERAGE_MOVING_RANGE).toStrictEqual(expected);
     })
 });
@@ -122,7 +121,7 @@ describe('Compute the Average Moving Range for the baseline', () => {
 describe('Compute the Lower Natural Process Limit to the result object', () => {
 
     test('have Lower Natural Process Limit to the result object', () => {
-        const result = pbc([]);
+        const result = computePBC([]);
         expect(result).toHaveProperty('LOWER_NATURAL_PROCESS_LIMIT');
     })
 
@@ -130,7 +129,7 @@ describe('Compute the Lower Natural Process Limit to the result object', () => {
         {data: [1, 1, 1, 1], expected: [1, 1, 1, 1]},
         {data: [1, 2, 1, 2], expected: [-1.16, -1.16, -1.16, -1.16]},
     ])('Compute Lower Natural Process Limit to the result object', ({data, expected}) => {
-        const result = pbc(data);
+        const result = computePBC(data);
         expect(result.LOWER_NATURAL_PROCESS_LIMIT).toStrictEqual(expected);
     })
 
